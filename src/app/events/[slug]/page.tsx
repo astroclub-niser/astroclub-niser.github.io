3c93026ac65564cb20133359b19acc8c4c74fc3e
gayatri-p <gaya3p13@gmail.com>
2024-01-11 05:31:02 +0530

    revert blogs

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -1,32 +1,33 @@
 import { Metadata } from "next"
 import fs from 'fs'
-import Markdown from "markdown-to-jsx";
-import matter from "gray-matter";
+// import Markdown from "markdown-to-jsx";
+// import matter from "gray-matter";
 
 export const metadata: Metadata = {
   title: 'Events | NISER Astronomy Club',
   description: 'We are the Astronomy Club of NISER, Bhubaneswar',
 }
 
-const getPostContent = (slug: string) => {
-  const folder = 'posts/'
-  const file = `${folder}${slug}.md`
-  const content = fs.readFileSync(file, 'utf8')
-  const matterResult = matter(content)
-  return matterResult
-}
+// const getPostContent = (slug: string) => {
+//   const folder = 'posts/'
+//   const file = `${folder}${slug}.md`
+//   const content = fs.readFileSync(file, 'utf8')
+//   const matterResult = matter(content)
+//   return matterResult
+// }
 
-export default async function PostPage(props: any) {
-  const slug = props.params.slug
-  const post = getPostContent(slug)
+export default async function PostPage() {
+  // const slug = props.params.slug
+  // const post = getPostContent(slug)
 
   return (
     <main className="project-post">
-      <h1>{post.data.title}</h1>
-      {/* <p>{post.data.date}</p> */}
+      {/* <h1>{post.data.title}</h1>
+       <p>{post.data.date}</p> 
       <article className="prose dark:prose-invert lg:prose-xl">
         <Markdown>{post.content}</Markdown>
-      </article>
-    </main>
+      </article>*/}
+      <p className="page-description">Page under construction.<br/>Come back later!</p>
+    </main> 
   )
 }
