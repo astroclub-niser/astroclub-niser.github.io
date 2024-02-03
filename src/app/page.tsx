754f8a6c8a22b350d94b2bf97dcbe9e63a43e62b
Gayatri <gaya3p13@gmail.com>
2024-02-03 12:24:16 +0530

    newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default async function Home() {
           <h3>The Monthly NAC Newsletter</h3>
           <p>The Astronomy Club Monthly Newsletter will be your ultimate passport to explore the furthest reaches of space, right from the comfort of your inbox!</p>
           <p>It will provide a curated collection of astronomical insights, celestial highlights, and educational resources that will ignite your passion for the mysteries of space. Whether you are a seasoned astronomer or simply fascinated by the cosmos, our newsletter is designed to cater to all levels of knowledge and enthusiasm.</p>
-          <button className='btn'><a target='_blank' href='https://drive.google.com/file/d/1y5bRlFItbBAT5j9ICuRiAE8K-VgqpDqF/view?usp=sharing'>Read Latest</a></button>
+          <button className='btn'><a target='_blank' href='https://drive.google.com/file/d/1tN4aYEysrj8TeM97y28Z6If_-Ir8flR6/view?usp=sharing'>Read Latest</a></button>
           <button className='btn'><a target='_blank' href='https://drive.google.com/drive/u/2/folders/1ObmIzxPWnGn2fG4HpaiMYrB3sjlxtvAl'>View All</a></button>
         </div>
       </div>
