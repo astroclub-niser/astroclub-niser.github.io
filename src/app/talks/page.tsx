298ab24001c2f32ef7120a38ac30b5b931b8443c
gayatri-p <gaya3p13@gmail.com>
2025-02-10 22:09:16 +0530

    test

--- src/app/talks/page.tsx
+++ src/app/talks/page.tsx
@@ -9,16 +9,18 @@ export const metadata: Metadata = {
   description: 'We are the Astronomy Club of NISER, Bhubaneswar',
 }
 
-
 export default async function Talks() {
+  // PROCESS DATA FROM TALKS.JSON FILE
   const talksFile = await fs.readFile(process.cwd() + '/data/talks.json', 'utf8');
   const talksData = JSON.parse(talksFile)
   const talkKeys = Object.keys(talksData)
-
+  
+  // SPLIT INTO 3 CATEGORIES
   const upcomingTalksData = talksData[talkKeys[0]]
   const facultyTalks = talksData[talkKeys[1]]
   const archivedTalks = talkKeys.slice(2)
-
+  
+  // EMPTY MESSAGE IF NO UPCOMING TALKS, ELSE FILL WITH TALK CARD
   let upcomingTalks = ''
   if (Object.keys(upcomingTalksData).length === 0) {
     upcomingTalks = 'There are no scheduled talks for now. Come back later!'
@@ -27,12 +29,13 @@ export default async function Talks() {
       <TalkCard key={id} title={title} date={date} speaker={speaker} speaker_desc={speaker_desc} poster={poster} youtubeID={youtubeID} abstract={abstract} />
     ))
   }
-
+  
   return (
     <main id="section-talks">
       <h1 className="page-heading">Talks</h1>
       <p className="page-description">An archive of all the talks conducted by the NAC.</p>
 
+      {/* UPCOMING TALKS */}
       <div className="talks-year" id={'upcoming-talks'}>
         <h2 className="talks-year-label team-heading">Upcoming Talks</h2>
         <div className="talks-grid section-description">
@@ -42,7 +45,8 @@ export default async function Talks() {
         </div>
       </div>
 
-      <div className="talks-year" id={'upcoming-talks'}>
+      {/* FACULTY TALKS */}
+      <div className="talks-year" id={'faculty-talks'}>
         <h2 className="talks-year-label team-heading">Faculty Talks</h2>
         <div className="talks-grid section-description">
           {facultyTalks.map(({ id, title, date, speaker, speaker_desc, poster, youtubeID, abstract }: TalkItemProps) => (
@@ -52,6 +56,7 @@ export default async function Talks() {
         </div>
       </div>
 
+      {/* YEAR WISE ARCHIVED TALKS */}
       {
         archivedTalks.map((year, index) => (
 
